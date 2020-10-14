e3fcc91b9823ff47d5698cd1c6d79f1b58343f01
robertoguerr <juntosnafe@gmail.com>
2020-10-13 23:02:39 -0300

    Projeto para correção01

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class LoginController extends Controller {
+
+    public function signin(){
+        echo 'Login';
+    }
+
+    public function signup(){
+        echo 'Cadastro';
+    }
+
+}
